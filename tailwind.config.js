2c3ae3b512c123eb2a7f8616135936a7cbc49b3a
Eduardo Paulos <edupaulos1@gmail.com>
2024-04-11 16:49:45 +0100

    feat: add colour scales

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      '01': '#FBFDFC',
+      '02': '#F8FAF9',
+      '03': '#F1F4F3',
+      '04': '#EBEFED',
+      '05': '#E6E9E8',
+      '06': '#DEE3E0',
+      '07': '#D6DCD9',
+      '08': '#C2C9C6',
+      '09': '#A0ABAA',
+      10: '#859392',
+      11: '#637471',
+      12: '#111C18',
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
