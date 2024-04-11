9529bc9bf9dca832c86dedaf9bf582b8aebdab6d
Eduardo Paulos <edupaulos1@gmail.com>
2024-04-12 00:57:40 +0100

    chore: refactor painel control by adding a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import volumeUp from './assets/icons/volumeUp.svg';
 import volumeDown from './assets/icons/volumeDown.svg';
-import previousIcon from './assets/icons/previous.svg';
 import pauseIcon from './assets/icons/pause.svg';
 import playIcon from './assets/icons/play.svg';
-import nextIcon from './assets/icons/next.svg';
-import soundBar from './assets/soundBar.png';
-// import soundWave from './assets/sound.png';
+
 import { useSongs } from './hooks/useSongs';
+
 import SongWave from './components/SongWave';
+import PainelControl from './components/PainelControl';
 
 function App() {
   const {
@@ -38,37 +37,23 @@ function App() {
               <span className="text-3xl font-bold">{title}</span>
               <span className="text-3xl font-normal text-10">{artist}</span>
             </div>
-            <SongWave songUrl={songURL} isPlaying={isPlaying}></SongWave>
           </div>
         </div>
         <div className="absolute bottom-0 left-0 right-0 border-t border-t-05 bg-02 p-[1.6rem]">
           <div className="flex items-center justify-center gap-5">
             <img src={volumeDown} alt="" />
-            <img className="h-[0.6rem]" src={soundBar} alt="" />
+
             <img src={volumeUp} alt="" />
           </div>
         </div>
       </div>
-      <div className="drop-shadow-custom mt-6 inline-flex gap-6 rounded-3xl border border-05 bg-01 p-4">
-        <button
-          className="flex size-16 items-center justify-center rounded-xl text-8xl hover:bg-05 active:bg-08"
-          onClick={prevSong}
-        >
-          <img src={previousIcon} alt="Previous Button" />
-        </button>
-        <button
-          className="flex size-16 items-center justify-center text-8xl"
-          onClick={togglePlay}
-        >
-          <img src={isPlaying ? pauseIcon : playIcon} alt={altPlayBtn} />
-        </button>
-        <button
-          className="flex size-16 items-center justify-center rounded-xl text-8xl hover:bg-05 active:bg-08"
-          onClick={nextSong}
-        >
-          <img src={nextIcon} alt="Next Button" />
-        </button>
-      </div>
+      <PainelControl
+        isPlaying={isPlaying}
+        prevSong={prevSong}
+        togglePlay={togglePlay}
+        nextSong={nextSong}
+        altPlayBtn={altPlayBtn}
+      />
     </div>
   );
 }
