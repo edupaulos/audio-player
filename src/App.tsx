2dae79f80660fe18ee0dd8c56779bde724111050
Eduardo Paulos <edupaulos1@gmail.com>
2024-04-11 18:10:56 +0100

    feat: adjust UI to useSongs hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,32 @@ import volumeUp from './assets/icons/volumeUp.svg';
 import volumeDown from './assets/icons/volumeDown.svg';
 import previousIcon from './assets/icons/previous.svg';
 import pauseIcon from './assets/icons/pause.svg';
+import playIcon from './assets/icons/play.svg';
 import nextIcon from './assets/icons/next.svg';
 import soundBar from './assets/soundBar.png';
 import soundWave from './assets/sound.png';
-import cover from './assets/music.svg';
+import { useSongs } from './hooks/useSongs';
 
 function App() {
+  const { cover, title, album, isPlaying, prevSong, nextSong, togglePlay } =
+    useSongs();
+
+  const altPlayBtn = isPlaying ? 'Pause song button' : 'Play song button';
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <div className="relative mx-auto h-[22rem] w-[60rem] overflow-hidden rounded-3xl border border-05 bg-01 p-4">
         <div className="mb-[1.6rem] inline-flex gap-7 ">
           <div className="relative flex items-center justify-center">
             <img className="size-[14rem] rounded-lg" src={cover} />
-            <button className="absolute" onClick={() => {}}>
-              <img src={pauseIcon} alt="Pause Button" />
+            <button className="absolute" onClick={togglePlay}>
+              <img src={isPlaying ? pauseIcon : playIcon} alt={altPlayBtn} />
             </button>
           </div>
           <div className="flex flex-col gap-11">
             <div className="flex flex-col">
-              <span className="text-3xl font-bold">Yègellé Tezeta</span>
-              <span className="text-3xl font-normal">Mulatu Astatke</span>
+              <span className="text-3xl font-bold">{title}</span>
+              <span className="text-3xl font-normal">{album}</span>
             </div>
             <img className="" src={soundWave} />
           </div>
@@ -37,19 +43,19 @@ function App() {
       <div className="mt-6 inline-flex gap-6 rounded-3xl border border-05 bg-01 p-4">
         <button
           className="flex size-16 items-center justify-center text-8xl"
-          onClick={() => {}}
+          onClick={prevSong}
         >
           <img src={previousIcon} alt="Previous Button" />
         </button>
         <button
           className="flex size-16 items-center justify-center text-8xl"
-          onClick={() => {}}
+          onClick={togglePlay}
         >
-          <img src={pauseIcon} alt="Pause or Button" />
+          <img src={isPlaying ? pauseIcon : playIcon} alt={altPlayBtn} />
         </button>
         <button
           className="flex size-16 items-center justify-center text-8xl"
-          onClick={() => {}}
+          onClick={nextSong}
         >
           <img src={nextIcon} alt="Next Button" />
         </button>
